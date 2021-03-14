0f49deb86574e6cedcc018e6a7391f7808a1aa7e
Edward J. C. Ashenbert <nguyenquangbinh803@gmail.com>
2021-03-15 07:33:02 +0900

    Update ExcelGenerator and DirectoryExplore

---
+++ DirectoryExplore.py
@@ -0,0 +1,54 @@
+from ExcelGenerator import ExcelGenerator
+import os
+import re
+
+
+class DirectoryExplore:
+    def __init__(self, directory_path):
+        self.directory_path = directory_path
+
+    def patse_directory(self, file_name):
+        excel_generator = ExcelGenerator(file_name)
+        browse_count = 0
+        directory_count = 0
+        for current_directory, sub_directory, files in os.walk(self.directory_path):
+            # print(os.path.basename(current_directory), sub_directory, files)
+            # print(current_directory)
+            browse_count += 1
+            print(current_directory)
+            if browse_count != 1:
+                for sub in sub_directory:
+                    directory_count += 1
+                    layer_list = os.path.join(
+                    current_directory, sub).replace("\\", "/").split("/")
+                
+                    for index, layer in enumerate(layer_list):
+                        excel_generator.insert_cell_value(
+                            directory_count, index + 1, layer)
+                        excel_generator.insert_cell_hyperlink(
+                        directory_count, len(layer_list) - index, os.path.normpath(os.path.join(current_directory, sub) + (os.path.sep + os.path.pardir)*(index)))
+
+            for file in files:
+                directory_count += 1
+                layer_list = os.path.join(
+                    current_directory, file).replace("\\", "/").split("/")
+                for index, layer in enumerate(layer_list):
+                    excel_generator.insert_cell_value(
+                        directory_count, index + 1, layer)
+                    excel_generator.insert_cell_hyperlink(
+                        directory_count, len(layer_list) - index, os.path.normpath(os.path.join(current_directory, file) + (os.path.sep + os.path.pardir)*(index)))
+
+            # break
+if __name__ == '__main__':
+    directory_explore = DirectoryExplore("C:/Users/ASUS/Documents")
+    directory_explore.patse_directory("test/02_test_patse_result.xlsx")
+    # layer_1 = ['A', 'B', "C"]
+    # layer_2 = [['A',"D","D","D","D"], ["D","D","D","D"]]
+    # layer_3 = [[['','R'], ['','R']], [['','R'], ['','R']]]
+    # layer = []
+    # layer.append(layer_1)
+    # layer.append(layer_2)
+    # layer.append(layer_3)
+
+    # Base on the number of leyer we will take our collumn apparently
+    # print(layer[2][0][1])
---
+++ ExcelGenerator.py
@@ -0,0 +1,45 @@
+# Author
+import openpyxl
+import os
+
+
+class ExcelGenerator:
+    def __init__(self, file_name):
+        if not os.path.exists(file_name):
+            self.workbook = openpyxl.Workbook()
+            try:
+                self.workbook.save(file_name)
+            except FileNotFoundError as exp:
+                container_path = os.path.normpath(
+                    file_name + os.path.sep + os.path.pardir)
+                os.mkdir(container_path)
+                self.workbook.save(file_name)
+        else:
+            self.workbook = openpyxl.load_workbook(file_name)
+        self.file_name = file_name
+        self.sheet = self.workbook.active
+
+    def query_cell_value(self, cell_row, cell_column):
+        return self.sheet.cell(row=cell_row, column=cell_column).value
+
+    def insert_cell_value(self, cell_row, cell_column, cell_value):
+        self.sheet.cell(row=cell_row, column=cell_column).value = cell_value
+        
+        return self.sheet.cell(row=cell_row, column=cell_column).value
+
+    def insert_cell_hyperlink(self, cell_row, cell_column, cell_link):
+        self.sheet.cell(row=cell_row, column=cell_column).hyperlink = cell_link
+        
+        return self.sheet.cell(row=cell_row, column=cell_column).hyperlink
+
+    def save_workbook(self):
+        self.workbook.save(self.file_name)
+        
+    def insert_row(self, row) :
+        self.sheet.insert_row(row)
+
+if __name__ == '__main__':
+    excel_file = ExcelGenerator("test/01_test_file.xlsx")
+    # print(type(excel_file.insert_cell_value(1, 1, 12)))
+    print(excel_file.query_cell_value(1, 1))
+    print(excel_file.insert_cell_hyperlink(1, 1, os.path.normpath("C:/Users\ASUS\Desktop/2021-Projects\Python-Assett/02-Libraries-Implementation\excel").replace(os.path.sep, '/')))
