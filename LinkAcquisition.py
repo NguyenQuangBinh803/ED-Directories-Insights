d36a9b12fdc7f31c62df29c3a5dfce47d7e27572
グェン クァン ビン <binh_nguyen@adtec-eng.co.jp>
2021-03-26 08:41:31 +0900

    Payload Acquisition

--- LinkAcquisition.py
+++ LinkAcquisition.py
@@ -15,5 +15,22 @@ SIGN_FILE_EMPTY = "ファイルがありません。"
 file_not_empty = "id=\"js-filemanage-view-area\""
 # This is division id
 # data inside <tr class=js-scroll-pos listview-item-row   ----> Loop all same class name for query
+
+import re
+from bs4 import BeautifulSoup as bs
+
+import requests
+from requests.auth import HTTPDigestAuth
+
+
 if __name__ == "__main__":
-    pass
+    response = requests.get("http://cybozu.adtec-eng.co.jp/cb7/ag.exe?page=FileIndex", auth = HTTPDigestAuth('binh_nguyen', 'adtub684'))
+    print(response.content.decode("utf-8"))
+
+csrf_ticket:
+_System: login
+_Login: 1
+LoginMethod: 2
+_Account: binh_nguyen
+Password: adtub684
+Submit: ログイン
